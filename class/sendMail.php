df7cd77c928f4a6e04d7a8fe31c8a104fef59e5a
tovo <tovo.rakotoson@antennereunion.fr>
2019-04-18 14:44:51 +0300

    mise à jour logo partenaire et formulaire envoie mail

---
+++ class/sendMail.php
@@ -0,0 +1,39 @@
+<?php
+require("/PHPMailer/src/PHPMailer.php");
+require("/PHPMailer/src/SMTP.php");
+require("/PHPMailer/src/Exception.php");
+
+
+$messageError = "";
+$messageSuccess = "";
+
+if (isset($_POST["action"]) && $_POST["action"] == "ok") {
+
+$mail = new \PHPMailer\PHPMailer\PHPMailer();
+    $mail->isHTML(true);
+
+    $mail->From = "noreply@rodzafer.fr";
+    $mail->FromName = $_POST["nom"] . " " . $_POST["prenom"];
+    $mail->addAddress("geraldo.raobelinarisoa@antennereunion.fr");     // Add a recipient
+
+    $elm = "";
+
+    $elm .= "<p>Nom: " . ucfirst($_POST["nom"]) . "</p>";
+    $elm .= "<p>Prenom: " . ucfirst($_POST["prenom"]) . "</p>";
+
+    $elm .= "<p>Email: " . $_POST["email"] . "</p>";
+
+    $elm .= "<p>Tel: " . $_POST["phone"] . "</p>";
+    $elm .= "<p>Message: " . $_POST["description"] . "</p>";
+
+
+    $mail->Subject = "Rodzafer contact";
+    $mail->Body    = $elm;
+
+    if(!$mail->send()) {
+        $messageSuccess = "Votre message a été bien envoyé.";
+    } else {
+        $messageError = "Votre message n'a pas pu être envoyé.";
+    }
+
+}
